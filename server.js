89f5aa301a0b67e050c6ff9a558c908f393e0dbc
Lindsay <lbarnett712@gmail.com>
2020-12-18 08:18:39 -0500

    feat - added bd connection

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
 const port = process.env.PORT || 1234
 
-app.get('/api', (req, res) => {
-    console.log('heard')
-  res.json([
-    {id: 1, title: 'hello, this is from server.js'}
-  ])
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MogoDB database connection established successfully')
 })
 
+// app.get('/api', (req, res) => {
+//     console.log('heard')
+//   res.json([
+//     {id: 1, title: 'hello, this is from server.js'}
+//   ])
+// })
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server is running at http://localhost:${port}`)
 })
