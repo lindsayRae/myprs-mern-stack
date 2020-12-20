d52e30f48914c9e6d4c637310f3c6a5fe0ebbe6e
Lindsay <lbarnett712@gmail.com>
2020-12-19 20:44:58 -0500

    feat - added lodash, joi, & bcrypt

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,8 @@
+const Joi = require('joi')
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const userSchema =  new Schema({
+
+const User = mongoose.model('User', new mongoose.Schema({
     userName: {
         type: String,
         required: true,
@@ -27,8 +28,17 @@ const userSchema =  new Schema({
 }, 
 {
     timestamps: true
-});
+}));
 
-const User = mongoose.model('User', userSchema);
+function validateUser(user) {
+    const schema =  Joi.object({
+        userName: Joi.string().min(1).max(99).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(8).max(255).required()
+    })
+    return Joi.validate(user, schema);
+}
 
-module.exports = User;
+
+exports.User = User;
+exports.validate = validateUser;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
-const router = require('express').Router();
-let User = require('../models/user.model');
+const bcrypt = require('bcrypt')
+const _ = require('lodash'); 
+const {User, validate} = require('../models/user.model');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
 
 router.route('/').get((req, res) => {
     User.find()
@@ -7,16 +12,33 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err))
 })
 
-router.route('/add').post((req, res) => {
-    const userName = req.body.userName;    
-    const email = req.body.email;
-    const password = req.body.password;
+// router.route('/add').post((req, res) => {
+//     const userName = req.body.userName;    
+//     const email = req.body.email;
+//     const password = req.body.password;
     
-    const newUser = new User({userName, email, password })
+//     const newUser = new User({userName, email, password })
 
-    newUser.save()
-        .then(() => res.json('User added!'))
-        .catch(err => res.status(400).json('Error: ' + err))
+//     newUser.save()
+//         .then(() => res.json('User added!'))
+//         .catch(err => res.status(400).json('Error: ' + err))
+// })
+
+router.post('/add', async (req, res) =>{
+    const {error} = validate(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+
+    let user = await User.findOne({email: req.body.email})
+    if(user) return res.status(400).send('User already registered.')
+    
+    // _.pick lets us decided what specifically we want to send to DB and back to the client 
+    user = new User (_.pick(req.body, ['userName', 'email', 'password']))
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(user.password, salt)
+
+    await user.save()   
+    
+    res.send(_.pick(user, ['userName', 'email']))
 })
 
 module.exports = router;
