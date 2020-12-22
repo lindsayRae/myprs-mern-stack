d4d8e13022f6c6deed58f5be7e5e170339d9b681
Lindsay <lbarnett712@gmail.com>
2020-12-22 08:33:35 -0500

    chore - refactor

--- routes/admin.js
+++ routes/admin.js
@@ -2,40 +2,23 @@ const { Movement, validate } = require('../models/movement.model');
 
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
 
 /**
  * @description creates preDefined Movements for the DB, only for the developer
  */
 router.post('/defaults', async (req, res) => {
     
-    let body = {
-        name: req.body.name,
-        type: req.body.type,
-        preDefined: req.body.preDefined
-    }
- 
-    try {
-      
-        let validated = validate(body);
-     
-        if (validated.error) {
-            let errorMsg = validated.error.details[0].message
-            return res.status(400).send(errorMsg);
-        } 
+    const {error} = validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
 
-        let movement = new Movement({
-            name: req.body.name,
-            type: req.body.type,
-            preDefined: req.body.preDefined
-        })
-       
-        let result = await movement.save() 
-       
-        res.send(result)
+    let movement = await Movement.findOne({name: req.body.name})
+    if(movement) return res.status(400).send('Movement already exists.')
 
-    } catch (error) {  
-        console.error(error)     
-    }
+    movement =  new Movement (_.pick(req.body, ['name', 'type', 'preDefined']))
+
+    let result = await movement.save()
+    res.send(result); 
 });
 
 module.exports = router;
