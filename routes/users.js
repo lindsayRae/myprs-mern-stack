991cc0adee792eb7e738b588b5d597902d754917
Lindsay <lbarnett712@gmail.com>
2020-12-22 07:41:29 -0500

    chore- refactor to use error handling package

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-const Joi = require('joi')
+const Joi = require('joi');
 const auth = require('../middleware/auth');
 const bcrypt = require('bcrypt');
 const _ = require('lodash'); 
@@ -6,14 +6,14 @@ const {User, validate} = require('../models/user.model');
 const express = require('express');
 const router = express.Router();
 
+// Note:  All error handling is used through "express-async-errors": "^3.1.1"
+
 /**
  * @description GET all users
  */
 router.get('/', async (req, res) => {
-         
-    User.find()
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json('Error: ' + err))
+    const users = await User.find(); 
+    res.send(users);
 })
 
 /**
@@ -30,33 +30,24 @@ router.get('/me', auth, async (req, res) => {
 router.put('/me', auth, async (req, res) => {
     const {error} = validateUser(req.body)
     if(error) return res.status(400).send(error.details[0].message);
-
-    try {
-        const user = await User.findByIdAndUpdate(req.user._id, {
-            $set: {
-                userName: req.body.userName,
-                email: req.body.email
-            }
-        }, {new: true});
-        if(user) return res.send(user); 
-        if(!user) return res.send('Could not update user.')
-    } catch (error) {
-        let result = error.message
-        res.send(result)
-    }      
+   
+    const user = await User.findByIdAndUpdate(req.user._id, {
+        $set: {
+            userName: req.body.userName,
+            email: req.body.email
+        }
+    }, {new: true});
+    if(user) return res.send(user); 
+    if(!user) return res.send('Could not update user.')
+       
 })
 
 /**
  * @description DELETE current user
  */
-router.delete('/me', auth, async(req, res) => {
-    try {
-        const result = await User.deleteOne({_id: req.user._id})     
-        res.send(result)
-    } catch (error) {
-        let result = error.message
-        res.send(result)
-    }
+router.delete('/me', auth, async(req, res) => {    
+    const result = await User.deleteOne({_id: req.user._id})     
+    res.send(result)    
 })
 
 /**
@@ -65,25 +56,19 @@ router.delete('/me', auth, async(req, res) => {
 router.post('/', async (req, res) =>{
     const {error} = validate(req.body)    
     if(error) return res.status(400).send(error.details[0].message)
+ 
+    let user = await User.findOne({email: req.body.email})
+    if(user) return res.status(400).send('User already registered.')
+    
+    // _.pick lets us decided what specifically we want to send to DB and back to the client 
+    user = new User (_.pick(req.body, ['userName', 'email', 'password']))
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(user.password, salt)
 
-    try {
-        let user = await User.findOne({email: req.body.email})
-        if(user) return res.status(400).send('User already registered.')
-        
-        // _.pick lets us decided what specifically we want to send to DB and back to the client 
-        user = new User (_.pick(req.body, ['userName', 'email', 'password']))
-        const salt = await bcrypt.genSalt(10)
-        user.password = await bcrypt.hash(user.password, salt)
+    await user.save()   
 
-        await user.save()   
-
-        const token = user.generateAuthToken();
-        res.header('x-auth-token', token).send(_.pick(user, ['userName', 'email']))
-        
-    } catch (error) {
-        let result = error.message
-        res.send(result)
-    }
+    const token = user.generateAuthToken();
+    res.header('x-auth-token', token).send(_.pick(user, ['userName', 'email']))        
     
 })
 
