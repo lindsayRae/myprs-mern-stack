3c111285d6d070b808780a0877fd9f1c7842fce0
Lindsay <lbarnett712@gmail.com>
2020-12-31 07:25:09 -0500

    chore - fixed spacing

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,27 +1,22 @@
 // https://www.youtube.com/watch?v=SmMZqh1xdB4
 
 
-import React, {useState, forwardRef, useImperativeHandle} from 'react'
-import ReactDOM from 'react-dom'
+import React, {useState, forwardRef, useImperativeHandle} from 'react';
+import ReactDOM from 'react-dom';
 
 const Modal =  forwardRef((props, ref) => {
-    const [display, setDisplay] = useState(false)
+    const [display, setDisplay] = useState(false);
 
     useImperativeHandle(ref, () => {
         return {
             openModal: () => open(),
             closeModal: () => close()
         }
-    } )
-    const open = () => {
-        setDisplay(true)
-    }
-    const close = () => {
-        setDisplay(false)
-    }
+    });
+    const open = () => setDisplay(true); 
+    const close = () => setDisplay(false);
 
     if(display){
-
        return ReactDOM.createPortal(
             <div className={'modal-wrapper'}>
                 <div className={'modal-backdrop'} onClick={close}/>
@@ -30,10 +25,8 @@ const Modal =  forwardRef((props, ref) => {
                 </div>
     
             </div>, document.getElementById('modal-root')
-        )
-      
+        )      
     }
-
     return null
     
 })
