2ce4ffd362947ae4e32a76c232533b967a3fa39a
Lindsay <lbarnett712@gmail.com>
2021-01-09 17:10:35 -0500

    chore - updated date length

--- models/personalRecord.model.js
+++ models/personalRecord.model.js
@@ -1,54 +1,51 @@
 const mongoose = require('mongoose');
 
-
 const movementSchema = new mongoose.Schema({
-        name: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 99
-        },
-        type: {
-            type: String,
-            required: true            
-        },        
-        preDefined: {
-            type: Boolean,
-            required: true            
-        },
-        date: {
-            required: true,
-            trim: true,
-            type: String,
-            maxlength: 999
-        },
-        comment: {
-            type: String,
-            required: false,            
-            maxlength: 999
-        },
-        personalRecord: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 99
-        }
-
-})
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 99,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  preDefined: {
+    type: Boolean,
+    required: true,
+  },
+  date: {
+    required: true,
+    trim: true,
+    type: String,
+    maxlength: 10,
+  },
+  comment: {
+    type: String,
+    required: false,
+    maxlength: 999,
+  },
+  personalRecord: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 99,
+  },
+});
 
 const PersonalRecordSchema = new mongoose.Schema({
-        user_id: {
-            type: String,
-            required: true,
-            minlength: 24,
-            maxlength: 24
-        },
-        skills: [movementSchema],
-        lifts: [movementSchema],
-        cardio: [movementSchema],
-})
+  user_id: {
+    type: String,
+    required: true,
+    minlength: 24,
+    maxlength: 24,
+  },
+  skills: [movementSchema],
+  lifts: [movementSchema],
+  cardio: [movementSchema],
+});
 
-const PersonalRecord = mongoose.model('PersonalRecord', PersonalRecordSchema)
-
+const PersonalRecord = mongoose.model('PersonalRecord', PersonalRecordSchema);
 
 exports.PersonalRecord = PersonalRecord;
