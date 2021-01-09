650890f5925da76dd95e0607ca3be9d322113659
Lindsay <lbarnett712@gmail.com>
2021-01-09 17:22:43 -0500

    removed update cardio option

--- client/src/components/AddCardio.js
+++ client/src/components/AddCardio.js
@@ -3,85 +3,89 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Redirect } from 'react-router-dom';
 
 const AddCardio = ({ cardio: propsCardio, addNewCardio, updateCardio }) => {
-    const [saved, setSaved] = useState(false);
-    const [cardio, setCardio] = useState({ ...propsCardio }) 
-    const [error, setError] = useState('')
+  const [saved, setSaved] = useState(false);
+  const [cardio, setCardio] = useState({ ...propsCardio });
+  const [error, setError] = useState('');
 
-    const prevCardioRef = useRef();
-    useEffect(() => {
-        prevCardioRef.current = cardio;
-    }, [cardio]);
-    const prevCardio = prevCardioRef.current;
-    
-    const handleAddCardio = (event) => {
-        event.preventDefault();
-        console.log(cardio)     
+  const prevCardioRef = useRef();
 
-        if (cardio.name && cardio.personalRecord){           
-            if(updateCardio){
-                updateCardio(cardio)
-            } else {
-                addNewCardio(cardio)
-            }                    
-            setSaved(true)
-        } else {
-            setError('Name and PR entry is required')
-        }                     
+  useEffect(() => {
+    prevCardioRef.current = cardio;
+  }, [cardio]);
+  // const prevCardio = prevCardioRef.current;
+
+  /**
+   * @description calls addNewCardio() from /pages/CardioList.js
+   *
+   */
+  const handleAddCardio = (event) => {
+    event.preventDefault();
+
+    if (cardio.name && cardio.personalRecord) {
+      addNewCardio(cardio);
+      setSaved(true);
+    } else {
+      setError('Name and PR entry are required');
     }
-    if(saved === true) {
-        return <Redirect to='/cardio' />
-    } 
-    return (
-        <form className='container' onSubmit={handleAddCardio}>           
-            <h1>{updateCardio ? 'Edit' : 'Add New'} Cardio</h1>                
-            <p>
-                <label htmlFor='form-name'>New Movement:</label>
-                <input                   
-                    id='form-name'
-                    value={cardio.name || ''}
-                    placeholder='Movement Name'
-                    onChange={event => {
-                        setError('')
-                        setCardio({
-                            ...cardio, 
-                            name: event.target.value
-                        })}
-                    }
-                />
-            </p>
-            <p>
-                <label htmlFor='form-pr'>PR:</label>               
-                <input                  
-                    id='form-pr'                    
-                    placeholder='Personal Record'
-                    value={cardio.personalRecord || ''}
-                    onChange={event => {
-                        setError('')
-                        setCardio({
-                            ...cardio, 
-                            personalRecord: event.target.value
-                        })}
-                    }
-                />
-            </p>
-            <p>
-                <label htmlFor='form-notes'>Notes:</label>               
-                <textarea                  
-                    id='form-notes'                    
-                    placeholder='Notes...'
-                    value={cardio.comment || ''}
-                    onChange={event => setCardio({
-                        ...cardio, 
-                        comment: event.target.value
-                    })}
-                />
-            </p>
-            <p>              
-                <button type='submit' className='linkLike'>Add</button>
-            </p>
-            {error && <p>{error}</p>}
-        </form>
-    )
-}
+  };
+  if (saved === true) {
+    return <Redirect to='/cardios' />;
+  }
+  return (
+    <form className='container' onSubmit={handleAddCardio}>
+      <h1>{updateCardio ? 'Edit' : 'Add New'} Cardio</h1>
+      <p>
+        <label htmlFor='form-name'>New Movement:</label>
+        <input
+          id='form-name'
+          value={cardio.name || ''}
+          placeholder='Movement Name'
+          onChange={(event) => {
+            setError('');
+            setCardio({
+              ...cardio,
+              name: event.target.value,
+            });
+          }}
+        />
+      </p>
+      <p>
+        <label htmlFor='form-pr'>PR:</label>
+        <input
+          id='form-pr'
+          placeholder='Personal Record'
+          value={cardio.personalRecord || ''}
+          onChange={(event) => {
+            setError('');
+            setCardio({
+              ...cardio,
+              personalRecord: event.target.value,
+            });
+          }}
+        />
+      </p>
+      <p>
+        <label htmlFor='form-notes'>Notes:</label>
+        <textarea
+          id='form-notes'
+          placeholder='Notes...'
+          value={cardio.comment || ''}
+          onChange={(event) =>
+            setCardio({
+              ...cardio,
+              comment: event.target.value,
+            })
+          }
+        />
+      </p>
+      <p>
+        <button type='submit' className='linkLike'>
+          Add
+        </button>
+      </p>
+      {error && <p>{error}</p>}
+    </form>
+  );
+};
 
-export default AddCardio
+export default AddCardio;
