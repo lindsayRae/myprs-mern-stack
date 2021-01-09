f6d8472b84ee82e3bcd15442e60fb2c438f34aea
Lindsay <lbarnett712@gmail.com>
2021-01-09 17:11:12 -0500

    chore - updated route

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-    <article className='not-found container'>
-        <h1>404!</h1>
-        <p>
-            Content not found.
-            <Link to='/' >Return to Dashboard</Link>
-        </p>
-    </article>
-)
+ <article className='not-found container'>
+  <h1>404!</h1>
+  <p>
+   Content not found.
+   <Link to='/dashboard'>Return to Dashboard</Link>
+  </p>
+ </article>
+);
 
 export default NotFound;
