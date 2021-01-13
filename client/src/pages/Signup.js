94bad3920d6623b8da2204f9fc1548a4f6bbb2cf
Lindsay <lbarnett712@gmail.com>
2021-01-13 07:03:46 -0500

    chore - cleanup and removing localStorage set

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,93 +1,90 @@
-import React, {useState, useContext, useEffect} from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { UserContext } from '../context/UserContext';
 
-export default({history}) => {
-    
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [userName, setUserName] = useState('')
-    const [error, setError] = useState('')
+const Signup = ({ history }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [userName, setUserName] = useState('');
+  const [error, setError] = useState('');
 
-    const {user, setUser} = useContext(UserContext)
+  const { user, setUser } = useContext(UserContext);
 
-    useEffect(() => {        
-        console.log(user)
-        if(user){
-            history.push('/login')
-        } else {
-            history.push('/signup')
-        }
-    }, [user])
-
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-
-        try {
-            const response = await fetch('http://localhost:1234/api/users/register', {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify({
-                    userName,
-                    email,
-                    password
-                })
-            })
-            
-            const data = await response.json();
-          
-            if(data.message){
-                setError(data.message)
-                return
-            } else {                
-                setUser(data)
-                localStorage.setItem("UserID", data._id);
-                localStorage.setItem("jwt", data.token);
-            }
-        } catch (err) {
-            setError('Something went wrong: ', err)
-        }
+  useEffect(() => {
+    console.log(user);
+    if (user) {
+      history.push('/login');
+    } else {
+      history.push('/signup');
     }
+  }, [user]);
 
-    return (
-        <div>
-            
-            <form  className='container' onSubmit={handleSubmit}>
-                Signup
-                <p>
-                    <label htmlFor='userName'>User Name:</label>
-                    <input 
-                        type='string'
-                        value={userName}
-                        onChange={(event) => setUserName(event.target.value)}
-                    />
-                </p>
-                <p>
-                    <label htmlFor='email'>Email:</label>
-                    <input 
-                        type='email'
-                        value={email}
-                        onChange={(event) => setEmail(event.target.value)}
-                    />
-                </p>
-                <p>
-                    <label htmlFor='password'>Password:</label>
-                    <input 
-                        type='password'
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
-                    />
-                </p>
-                <p>
-                    <button type='submit' className='linkLike'>Signup</button> 
-                </p>
-                 
-            </form>
-            
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-            {error && <p>{error}</p>}
-        </div>
-    )
-    
-}
+    try {
+      const response = await fetch('http://localhost:1234/api/users/register', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          userName,
+          email,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (data.message) {
+        setError(data.message);
+        return;
+      } else {
+        setUser(data);
+      }
+    } catch (err) {
+      setError('Something went wrong: ', err);
+    }
+  };
+
+  return (
+    <div>
+      <form className='container' onSubmit={handleSubmit}>
+        Signup
+        <p>
+          <label htmlFor='userName'>User Name:</label>
+          <input
+            type='string'
+            value={userName}
+            onChange={(event) => setUserName(event.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor='email'>Email:</label>
+          <input
+            type='email'
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor='password'>Password:</label>
+          <input
+            type='password'
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </p>
+        <p>
+          <button type='submit' className='linkLike'>
+            Signup
+          </button>
+        </p>
+      </form>
+
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default Signup;
