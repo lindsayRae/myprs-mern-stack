47b54fcc8e1993a79ad4a0fc730e23091343b970
Lindsay <lbarnett712@gmail.com>
2021-01-13 07:01:53 -0500

    chore - cleanup and removing localStorage set

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,76 +1,83 @@
 import React, { useState, useContext, useEffect } from 'react';
-import {UserContext} from '../context/UserContext';
+import { UserContext } from '../context/UserContext';
 
-// eslint-disable-next-line
-export default ({history}) => {
- //   const { onLogin } = useContext(UserContext)
+const Login = ({ history }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+  const { user, setUser } = useContext(UserContext);
 
-    const {user, setUser} = useContext(UserContext)
-    console.log('user', user)
-
-    useEffect(() => {
-        if(user){
-            history.push('/dashboard')
-        }
-    }, [user])
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();      
-       try {
-            const response = await fetch('http://localhost:1234/api/auth', {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify({
-                    email,
-                    password
-                })
-            })
-    
-            const data = await response.json()
-            console.log('data', data)
-    
-            if(data.message){
-                setError(data.message)
-                return
-            }
-            console.log(data)
-            localStorage.setItem('UserID', data.user._id)
-            localStorage.setItem('jwt', data.jwt)
-            setUser(data)
-       } catch (err) {
-           setError(`Something went wrong: ${err}`)
-       }
+  useEffect(() => {
+    if (user) {
+      history.push('/dashboard');
     }
+  }, [user]);
 
-    return (
-        <div>
-            <form className='container' name='login' onSubmit={handleSubmit}>
-                <p>
-                    <label htmlFor='email'>Email:</label>
-                    <input type='email' value={email} onChange={(event) => {
-                        setError('')
-                        setEmail(event.target.value)
-                    }} />
-                </p>
-                <p>
-                    <label htmlFor='password'>Password:</label>
-                    <input type='password' value={password} onChange={(event) => {
-                        setError('')
-                        setPassword(event.target.value)
-                    }} />
-                </p>
-                <p>
-                    <button type='submit' disabled={!email && !password} className='linkLike'>Login</button>
-                </p>
-            </form>
-            {error && <p>{error}</p>}
-        </div>
-        
-    )
-}
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://localhost:1234/api/auth', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (data.message) {
+        setError(data.message);
+        return;
+      }
+      setUser(data);
+    } catch (err) {
+      setError(`Something went wrong: ${err}`);
+    }
+  };
+
+  return (
+    <div>
+      <form className='container' name='login' onSubmit={handleSubmit}>
+        <p>
+          <label htmlFor='email'>Email:</label>
+          <input
+            type='email'
+            value={email}
+            onChange={(event) => {
+              setError('');
+              setEmail(event.target.value);
+            }}
+          />
+        </p>
+        <p>
+          <label htmlFor='password'>Password:</label>
+          <input
+            type='password'
+            value={password}
+            onChange={(event) => {
+              setError('');
+              setPassword(event.target.value);
+            }}
+          />
+        </p>
+        <p>
+          <button
+            type='submit'
+            disabled={!email && !password}
+            className='linkLike'
+          >
+            Login
+          </button>
+        </p>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default Login;
