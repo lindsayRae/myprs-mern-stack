b5305276ae3aae269ebd7df3c0c4f9c0c232d478
Lindsay <lbarnett712@gmail.com>
2021-01-25 06:31:02 -0500

    feat - started stying for single moement page

--- client/src/pages/SingleMovement.js
+++ client/src/pages/SingleMovement.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { UserContext } from '../context/UserContext';
-
-const SingleMovement = ({ match }) => {
+import {
+  MdKeyboardBackspace,
+  MdAdd,
+  MdCancel,
+  MdModeEdit,
+} from 'react-icons/md';
+import './SingleMovement.css';
+const SingleMovement = ({ history, match }) => {
   const { id } = match.params;
   const name = id.replace(/-/g, ' ');
 
@@ -11,11 +17,15 @@ const SingleMovement = ({ match }) => {
   const [edit, setEdit] = useState(false);
   const [selectedEntry, setSelectedEntry] = useState('');
   const [editPR, setEditPR] = useState('');
-  const [newPR, setNewPR] = useState('');
   const [editDate, setEditDate] = useState('');
-  const [newDate, setNewDate] = useState('');
   const [editComment, setEditComment] = useState('');
+
+  const [newPR, setNewPR] = useState('');
+  const [newDate, setNewDate] = useState(new Date().toISOString().slice(0, 10));
   const [newComment, setNewComment] = useState('');
+
+  console.log('newPR', newPR);
+  console.log('newComment', newComment);
   const { user } = useContext(UserContext);
 
   useEffect(() => {
@@ -199,113 +209,165 @@ const SingleMovement = ({ match }) => {
     }
   };
 
+  const formatDate = (date) => {
+    let dateArr = date.split('-');
+    return `${dateArr[1]}-${dateArr[2]}-${dateArr[0]}`;
+  };
   return (
-    <div>
-      <h2 className='capitalize'>{name}</h2>
-      <div>
-        {loading && <p>Loading...</p>}
-        {!loading && (
-          <>
-            {!entries.length && (
-              <>
-                <p>No records yet for this movement, get moving!</p>
-              </>
-            )}
+    <div className='page-splash'>
+      <header className='header header-fixed'>
+        <div className='header-inner' onClick={() => history.goBack()}>
+          <div className='navbar'>
+            <a className='menu-bars'>
+              <MdKeyboardBackspace className='text-info' />
+            </a>
+            <h3 className='capitalize page-title'>{name}</h3>
+            <div style={{ width: '26px' }}> </div>
+          </div>
+        </div>
+        <div className='img-container overlay'>
+          <img
+            src='../images/row.jpg'
+            alt='cardio'
+            className='img-responsive'
+          />
+          <div className='add-container'>
+            <form className='' onSubmit={handleNewSubmit}>
+              <div className='inline-form-group'>
+                <div className='inline-form'>
+                  <input
+                    id='new-pr-entry'
+                    value={newPR}
+                    type='number'
+                    onChange={(event) => {
+                      console.log(event);
+                      setError('');
+                      setNewPR(event.target.value);
+                    }}
+                    required
+                  />
+                  <label htmlFor='new-pr-entry' className='modal-label-name'>
+                    <span className='modal-content-name'>PR:</span>
+                  </label>
+                </div>
+                <div className='inline-form'>
+                  <input
+                    id='new-date'
+                    value={newDate}
+                    type='date'
+                    onChange={(event) => {
+                      console.log(event);
+                      setError('');
+                      setNewDate(event.target.value);
+                    }}
+                    required
+                  />
+                  <label htmlFor='new-date' className='modal-label-name'>
+                    <span className='modal-content-name light'></span>
+                  </label>
+                </div>
+              </div>
 
-            <ul>
-              {entries.length > 0 &&
-                entries.map((entry) => (
-                  <li key={entry._id} className='capitalize'>
-                    <p>
-                      PR: {entry.personalRecord}
-                      <span>Date: {entry.date}</span>
-                      <button
-                        className='linkLike'
-                        onClick={() => {
-                          handleDelete(entry._id, entry.type);
-                        }}
-                      >
-                        Delete
-                      </button>
-                      {'  '}
-                      <button
-                        className='linkLike'
-                        onClick={() => {
-                          setSelectedEntry(entry);
-                          setEditPR(entry.personalRecord);
-                          setEditDate(entry.date);
-                          setEdit(true);
-                        }}
-                        id={entry._id}
-                      >
-                        Edit
-                      </button>
-                    </p>
-                    {edit && selectedEntry._id === entry._id && (
-                      <form onSubmit={handleEditSubmit}>
-                        <input
-                          value={editPR}
-                          onChange={(event) => {
-                            setError('');
-                            setEditPR(event.target.value);
-                          }}
-                          placeholder='New PR'
-                        />
-                        <input
-                          value={editDate}
-                          onChange={(event) => {
-                            setError('');
-                            setEditDate(event.target.value);
-                          }}
-                          placeholder='New Date'
-                        />
-                        <textarea
-                          value={editComment}
-                          onChange={(event) => {
-                            setError('');
-                            setEditComment(event.target.value);
-                          }}
-                          placeholder='New Comment'
-                        />
-                        <button className='linkLike'>Save Change</button>
-                      </form>
-                    )}
-                  </li>
-                ))}
-            </ul>
-            <form onSubmit={handleNewSubmit}>
-              <input
-                value={newPR}
-                type='number'
-                onChange={(event) => {
-                  setError('');
-                  setNewPR(event.target.value);
-                }}
-                placeholder='New PR'
-              />
-              <input
-                value={newDate}
-                type='date'
-                onChange={(event) => {
-                  setError('');
-                  setNewDate(event.target.value);
-                }}
-                placeholder='New Date'
-              />
-              <textarea
-                value={newComment}
-                onChange={(event) => {
-                  setError('');
-                  setNewComment(event.target.value);
-                }}
-                placeholder='New Comment'
-              />
-              <button className='linkLike'>Add Entry</button>
+              <div className='modal-form light'>
+                <textarea
+                  id='new-comment'
+                  value={newComment}
+                  onChange={(event) => {
+                    setError('');
+                    setNewComment(event.target.value);
+                  }}
+                />
+                <label htmlFor='new-comment' className='modal-label-name'>
+                  <span className='modal-content-name'>Notes...</span>
+                </label>
+              </div>
+              <div className='inline-form-submit'>
+                <button type='submit' className='btn-text-icon'>
+                  <MdAdd style={{ fontSize: '24px' }} />
+                  <span>Add Entry</span>
+                </button>
+              </div>
             </form>
-          </>
-        )}
+          </div>
+        </div>
+      </header>
+      <div className='list-page'>
+        <div className='list'>
+          {loading && <p>Loading...</p>}
+          {!loading && (
+            <>
+              {!entries.length && (
+                <>
+                  <ul className='movements'>
+                    <li>No records yet for this movement, get moving!</li>
+                  </ul>
+                </>
+              )}
+
+              <ul className='movements'>
+                {entries.length > 0 &&
+                  entries.map((entry) => (
+                    <li key={entry._id} className='capitalize single-movement'>
+                      <div className='icon-container delete-container'>
+                        <MdCancel
+                          onClick={() => {
+                            handleDelete(entry._id, entry.type);
+                          }}
+                        />
+                      </div>
+                      <div className='icon-container edit-container'>
+                        <MdModeEdit
+                          onClick={() => {
+                            setSelectedEntry(entry);
+                            setEditPR(entry.personalRecord);
+                            setEditDate(entry.date);
+                            setEdit(true);
+                          }}
+                          id={entry._id}
+                        />
+                      </div>
+                      <span className='entry-container'>
+                        <span>PR: {entry.personalRecord}</span>
+                        <span>{formatDate(entry.date)}</span>
+                      </span>
+
+                      {edit && selectedEntry._id === entry._id && (
+                        <form onSubmit={handleEditSubmit}>
+                          <input
+                            value={editPR}
+                            onChange={(event) => {
+                              setError('');
+                              setEditPR(event.target.value);
+                            }}
+                            placeholder='New PR'
+                          />
+                          <input
+                            value={editDate}
+                            onChange={(event) => {
+                              setError('');
+                              setEditDate(event.target.value);
+                            }}
+                            placeholder='New Date'
+                          />
+                          <textarea
+                            value={editComment}
+                            onChange={(event) => {
+                              setError('');
+                              setEditComment(event.target.value);
+                            }}
+                            placeholder='New Comment'
+                          />
+                          <button className='linkLike'>Save Change</button>
+                        </form>
+                      )}
+                    </li>
+                  ))}
+              </ul>
+            </>
+          )}
+        </div>
+        {error && <p>{error}</p>}
       </div>
-      {error && <p>{error}</p>}
     </div>
   );
 };
