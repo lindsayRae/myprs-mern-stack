477dc1d70dcea9d6e80d9fc0f5f1886491cc9ef2
Lindsay <lbarnett712@gmail.com>
2021-02-04 07:59:25 -0500

    chore - formatting

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //const config = require('config');
 require('express-async-errors');
-const error = require('./middleware/error')
+const error = require('./middleware/error');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -8,42 +8,48 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 1234
+const port = process.env.PORT || 1234;
 
 const privateKey = process.env.prs_jwtPrivateKey;
 
 if (!privateKey) {
-  console.error('FATAL ERROR: jwtPrivateKey is not defined.')
+  console.error('FATAL ERROR: jwtPrivateKey is not defined.');
   // 0 is success, anything else is failure
-  process.exit(1)
+  process.exit(1);
 }
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 const connection = mongoose.connection;
 connection.once('open', () => {
-  console.log('MogoDB database connection established successfully')
-})
+  console.log('MogoDB database connection established successfully');
+});
 
 app.use(cors());
 app.use(express.json());
 
-const adminRouter = require('./routes/admin')
+const adminRouter = require('./routes/admin');
 const movementsRouter = require('./routes/movements');
 const usersRouter = require('./routes/users');
 const personalRecordsRouter = require('./routes/personalRecords');
 const auth = require('./routes/auth');
 const contact = require('./routes/email');
+const activate = require('./routes/activate');
 
-app.use('/api/admin', adminRouter)
+app.use('/api/admin', adminRouter);
 app.use('/api/movements', movementsRouter);
 app.use('/api/users', usersRouter);
-app.use('/api/prs', personalRecordsRouter)
+app.use('/api/prs', personalRecordsRouter);
 app.use('/api/auth', auth);
 app.use('/api/contact', contact);
+app.use('/api/activate', activate);
 
-app.use(error)
+app.use(error);
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
-})
+  console.log(`Server is running at http://localhost:${port}`);
+});
