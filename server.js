1c8c623936415a370a6fbe454df564afe60863dd
Lindsay <lbarnett712@gmail.com>
2021-03-05 07:16:57 -0500

    tesing https redirect

--- server.js
+++ server.js
@@ -65,7 +65,17 @@ app.use('/api/payments', paymentsRouter);
 app.use(error);
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(function (req, res, next) {
+    if (req.secure) {
+      // request was via https, so do no special handling
+      next();
+    } else {
+      // request was via http, so redirect to https
+      res.redirect('https://' + req.headers.host + req.url);
+    }
+  });
   // app.use(secure);
+
   app.use(express.static('public'));
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
